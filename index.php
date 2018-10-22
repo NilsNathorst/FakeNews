276974811d5e574bf740d333602f5eac52b11816
Nils <nilsnathorstwindahl@gmail.com>
2018-10-22 21:27:33 +0200

    basic setup for the app using search field to fetch articles from news api

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+require __DIR__.'/test.php';
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+  <link rel="stylesheet" href="main.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+  <title>Fake Nudes</title>
+</head>
+<body>
+  <div class="navbar-fixed">
+    <nav>
+      <div class="nav-wrapper ">
+        <form>
+          <div class="input-field">
+            <form action="index.php">
+              <input id="search" type="search" name="query" required>
+            </form>
+            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+            <i class="material-icons">close</i>
+
+          </div>
+        </form>
+      </div>
+    </nav>
+  </div>
+
+  <?php
+  for ($i = 0;
+  $i < 3;$i++):
+  ?>
+
+  <div class="card large">
+    <div class="card-image waves-effect waves-block waves-light">
+      <img src="<?= $data['articles'][$i]['urlToImage'];?>">
+    </div>
+  <div class="card-content">
+      <span class="card-title activator grey-text text-darken-4"><?= $data['articles'][$i]['title']; ?><i class="material-icons right">more_vert</i></span>
+      <p><?= $data['articles'][$i]['author']; ?> <?= $data['articles'][$i]['publishedAt']; ?></p>
+  </div>
+    <div class="card-reveal">
+      <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+      <p ><?= $data['articles'][$i]['content']; ?></p>
+      <p><a href="<?= $data['articles'][$i]['url']; ?>"><i class="material-icons right">add_circle</i></a></p>
+    </div>
+  </div>
+  <?php
+
+endfor;
+?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script type="text/javascript" src="libraries/p5.js"></script>
+<script type="text/javascript" src="libraries/p5.dom.js"></script>
+<!-- <script type="text/javascript" src="index.js"></script> -->
+</body>
+</html>
---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+ini_set("allow_url_fopen", '1');
+global $apiKey;
+global $today;
+global $search;
+$search = 'top-headlines';
+$apiKey = 'apiKey=4912411c6cb2430ebde2f47727915e33';
+
+$url = 'https://newsapi.org/v2/everything?q=' . $search . '&from=' . $today . '&sortBy=publishedAt&' . $apiKey;
+// $parsedUrl = loadJSON($url, $getData);
+
+$data = json_decode(file_get_contents($url), true);
+
+        $query = $_GET['query'];
+        $newUrl = 'https://newsapi.org/v2/everything?q=' . $query . '&from=' . $today . '&sortBy=publishedAt&' . $apiKey;
+        $data = json_decode(file_get_contents($newUrl), true);
+        $url = $newUrl
+?>
