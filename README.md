d08ab3c5cc4fda8410efc131dae05d173ce49dd1
Nils <nilsnathorstwindahl@gmail.com>
2018-10-31 15:06:45 +0100

    Rewrote the README.md

--- README.md
+++ README.md
@@ -1,5 +1,34 @@
-# FakeNews
-School assignment in PHP. Create a mock-news website.
+## FakeNews
+Our first awesome PHP-project! (first year web-dev @ [Yrgo](https://yrgo.se/utbildningar/media-och-kommunikation/webbutvecklare/))
+ - The assignment was to build a "fake-news" site including authors, articles, publish dates etc.. i chose to get real articles using an API, link below!
+
+## Motivation
+At first i wanted to build this using PDO/Sqlite3 but it turned out to be abit over my head. But i still wanted to make it abit more interesting than just arrays of articles i'd write myself.
 
 
-testers: Per Baltzar;
+## Tech/framework used
+<b>Built with</b>
+- [materializecss](https://materializecss.com/)
+- All JS, HTML, and PHP is vanilla.
+
+## Installation
+If you want to use this you will have to get your own API-key head over to:
+[Getting started with newsAPI](https://newsapi.org/docs/get-started)
+
+## API Reference
+- [API](https://newsapi.org/)
+	A simple API which gives you JSON-data with a bunch of cool articles based on search criteria.
+
+## How to use?
+For intended usage toggle device toolbar to iPhone X or similar.
+** Also only use "-" (dashes) in place of spaces in your searches!
+Otherwise it crashes **
+## Credits
+Thanks to:
+- [Per Baltzar](https://github.com/perbaltzar)
+- [Mathias Kristiansson](https://github.com/mrmakr)
+for testing.
+
+
+## License
+MIT © [Nattfrost](https://github.com/Nattfrost)
--- index.php
+++ index.php
@@ -1,64 +1,69 @@
 <?php
+declare(strict_types=1);
 require __DIR__.'/newsAPI.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-  <link rel="stylesheet" href="main.css">
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>!Fake News</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+	<link rel="stylesheet" href="main.css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>!Fake News</title>
 </head>
 
 <script type="text/javascript">
+//setting a default value to avoid "currentLikes is undefined" on first load.
 var currentLikes = 0;
 </script>
+
 <body>
-  <div class="navbar-fixed">
-    <nav>
-      <div class="nav-wrapper ">
-        <form method='get'>
-          <div class="input-field">
-            <form action="index.php">
-              <input id="search" type="search" name="query" required >
-            </form>
-            <label class="label-icon" for="search"><i class="material-icons">search</i></label>
-            <i class="material-icons">close</i>
-          </div>
-        </form>
-      </div>
-    </nav>
-  </div>
+	<div class="navbar-fixed">
+		<nav>
+			<div class="nav-wrapper ">
+				<form method='get'>
+					<div class="input-field">
+						<form action="index.php">
+							<input id="search" type="search" name="query" required >
+						</form>
+						<label class="label-icon" for="search"><i class="material-icons">search</i></label>
+						<i class="material-icons">close</i>
+					</div>
+				</form>
+			</div> <!-- /nav-wrapper -->
+		</nav>
+	</div>	<!-- /navbar-fixed -->
 
-  <?php
-  for ($i = 0;
-  $i < 3;$i++):
-  ?>
+	<?php
+	// "didn't have time" to make this into a function on the page. Planned to let the user choose how many articles to display.
+	$numberOfArticles = 5;
+	for ($i = 0;
+	$i < $numberOfArticles;$i++):
+	?>
 
-  <div class="card">
-    <div class="card-image waves-effect waves-block waves-light">
-      <img src="<?= $data['articles'][$i]['urlToImage'];?>">
-    </div>
-    <div class="card-content">
-      <span class="card-title activator grey-text text-darken-4"><?= $data['articles'][$i]['title']; ?><i class="material-icons right">more_vert</i></span>
-      <p><?= $data['articles'][$i]['author'].' , '.$data['articles'][$i]['source']['name'].'<br>'; ?> <?= substr($data['articles'][$i]['publishedAt'], 0, 10); ?></p>
-      <br>
-      <a  data-id="<?= 'likecounter'.$i?>"class ="button waves-effect waves-light red btn-floating likebutton"><i class="material-icons">favorite_border</i></a>
-      <span id="<?= 'likecounter'.$i?>">0</span>
-    </div>
-    <div class="card-reveal">
-      <span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
-      <p class="flow-text"><?= $data['articles'][$i]['content']; ?></p>
-      <p><a href="<?= $data['articles'][$i]['url']; ?>"><i class="material-icons right">add_circle</i></a></p>
-    </div>
-  </div>
-</div>
+	<div class="card">
+		<div class="card-image waves-effect waves-block waves-light">
+			<img src="<?= $data['articles'][$i]['urlToImage'];?>">
+		</div>
+		<div class="card-content">
+			<span class="card-title activator grey-text text-darken-4"><?= $data['articles'][$i]['title']; ?><i class="material-icons right">more_vert</i></span>
+			<p><?= $data['articles'][$i]['author'].' , '.$data['articles'][$i]['source']['name'].'<br>'; ?> <?= substr($data['articles'][$i]['publishedAt'], 0, 10); ?></p>
+			<br>
+			<a  data-id="<?= 'likecounter'.$i?>"class ="button waves-effect waves-light red btn-floating likebutton"><i class="material-icons">favorite_border</i></a>
+			<span id="<?= 'likecounter'.$i?>"></span>
+		</div>
+		<div class="card-reveal">
+			<span class="card-title grey-text text-darken-4"><i class="material-icons right">close</i></span>
+			<p class="flow-text"><?= $data['articles'][$i]['content']; ?></p>
+			<p><a href="<?= $data['articles'][$i]['url']; ?>"><i class="material-icons right">add_circle</i></a></p>
+		</div>
+	</div> <!--/.card  -->
 
-<?php
+
+	<?php
 endfor;
 ?>
 
